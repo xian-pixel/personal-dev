328f5db0fd2a12dc5c351a007d9ef5b231b3bc8d
zhengyang <xian120411@163.com>
2026-08-07 11:31:00 +0800

    移动启动类

---
+++ personal-system/src/main/java/com/zhengyang/PersonalDetailApplication.java
@@ -0,0 +1,13 @@
+package com.zhengyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDetailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalDetailApplication.class, args);
+    }
+
+}
